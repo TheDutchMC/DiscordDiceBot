60b17dbf97915c689274b4effc2a5bfe8d2c2a6b
TheDutchMC <github@thedutchmc.nl>
2020-09-30 15:25:06 +0200

    Added GatewayIntents

--- src/main/java/nl/thedutchmc/DiscordDiceBot/JdaHandler.java
+++ src/main/java/nl/thedutchmc/DiscordDiceBot/JdaHandler.java
@@ -1,11 +1,15 @@
 package nl.thedutchmc.DiscordDiceBot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import nl.thedutchmc.DiscordDiceBot.discordEvents.MessageReceivedEventListener;
 
 public class JdaHandler {
@@ -15,18 +19,24 @@ public class JdaHandler {
 	public TextChannel diceChannel;
 	
 	public void setupJda() {
-		JDABuilder builder = JDABuilder.createDefault(DiscordDiceBot.CONFIG.discordToken);
-		
-		builder.setActivity(Activity.playing("Rolling the dices"));
+				
+		List<GatewayIntent> intents = new ArrayList<>();
+		intents.add(GatewayIntent.GUILD_MESSAGES);
 		
 		try {
-			jda = builder.build();
+			jda = JDABuilder.createDefault(DiscordDiceBot.CONFIG.discordToken)
+					.setActivity(Activity.playing("Rolling the dices"))
+					.enableIntents(intents)
+					.build();
 			
 			jda.awaitReady();
+			
 		} catch (LoginException e) {
 			DiscordDiceBot.logWarn("There was an error logging in to Discord. Is your token correct?");
+			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+			
 		}
 		
 		jda.addEventListener(new MessageReceivedEventListener());
